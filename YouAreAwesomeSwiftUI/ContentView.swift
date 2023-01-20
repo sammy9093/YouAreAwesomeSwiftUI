d207cea2a47eedaa680382f89453537477ae7308
Samuel Ortega <samy9093@gmail.com>
2023-01-19 23:54:42 -0600

    Layered button view, scrolls through text, Caution: Needs to handle a larger array automatically

--- YouAreAwesomeSwiftUI/ContentView.swift
+++ YouAreAwesomeSwiftUI/ContentView.swift
@@ -6,27 +6,88 @@
 // Control+Option+click To pull available modifiers (attribute inspector accomplishes the same thing)
 //Views (elements of app user interface) Always
 //Modifiers accept input values
+// Option+Command+p Resume Preview
+//Command +/- Increases/Decreases CodeFont Size
+//Option Command [ (moves selection up one line)
+//Option Command ] does the down ^^
+//Shift Command L = Opens add Elements Dialog
+
+//Views being structs, structs being value types, value types being immutable
+//The underscore before a input in the modfier means the decleration has no label
+//If underscore is not present, the title: must be used
+// action: () -> Void //Really represents a block of code
+//() -> Void //Allows
+//Trailing Closure Format is less "clunky""
 
 import SwiftUI
 
 struct ContentView: View {
+    //var's must appear non-nested syntaxaly
+    var messageArray = ["You are Great!", "You are Awesome!", "This got fun!"]
+    
+    //@State prevents the destruction of the value of the view
+    //private keeps it within this struct
+    @State private var messageArrayContainer = ""
+    @State private var messageContainer = 0
+    //body is our parent view that is used to compile the screen!!
     var body: some View {
+        
+        //An HStack in a VStack, groovy...
         VStack {
-            Image(systemName: "lasso.and.sparkles")
-                .resizable()
-                .scaledToFit()
-                .padding([.bottom, .trailing])
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-                
-            Text("H Town, Peace up!")
+            
+            Text(messageArrayContainer) //Takes up the size of the text, hence why padding really shows contents of aPhysically viewed
             //After pressing enter and pressing "." auto indentation
                 .font(.largeTitle) //Just applied a modifier with an input value
                 .fontWeight(.black)
-            Image(systemName: "swift")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-                .colorInvert()
+                .padding()
+                .background(.red)
+            Text("H Town, Peace up!")
+                .font(.largeTitle)
+                .fontWeight(.black)
+            //.padding() //Blank padding is still padding of all around default system value 16
+                .background(.red)
+            HStack {
+                Button("Click me, (;") {
+                    if messageContainer < messageArray.count {
+                        
+                        messageArrayContainer = messageArray[messageContainer]
+                        messageContainer = messageContainer + 1
+                        print("\(messageContainer)")
+                    }
+                    else {
+                        messageContainer = 0
+                        messageArrayContainer = messageArray[messageContainer]
+                        messageContainer = messageContainer + 1
+                        print("\(messageContainer)")
+                    }
+                }
+                
+                Button("Not Used (:") {
+                    
+                }
+            } //buttonStyle a modifier to the Button Primitive view
+            .buttonStyle(.borderedProminent)
+                
+                
+                
+            HStack {
+                Image(systemName: "swift") //Modifier order return type validation string?
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor) //This modifier only applies to Any Type of View, imageScale returns a view from an Image
+                    .colorInvert()
+                Image(systemName: "lasso.and.sparkles")
+                    .resizable()
+                    .scaledToFit()
+                // Moved image away from lower view by 100 up
+                //Moved image away from trailing by 30
+                    .imageScale(.large) //Has no effect on resizable
+                //.padding([.bottom, .trailing], 6.0)
+                    .padding(.trailing, 30)
+                //.background(.black)
+                    .padding(.bottom, 100)
+                    .foregroundColor(.indigo)
+            }
+            
         }
         .padding()
     }
@@ -37,3 +98,6 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
--- YouAreAwesomeSwiftUI/YouAreAwesomeSwiftUIApp.swift
+++ YouAreAwesomeSwiftUI/YouAreAwesomeSwiftUIApp.swift
@@ -5,7 +5,7 @@
 //  Created by Sammy on 1/19/23.
 //
 // Cmnd + 0 //Toggle Show/Hide Navigator Pane // Hide Navigator found under View-> Navigators->Hide Navigator
-//ContentView() comes from ContentView and executes here.
+//
 import SwiftUI
 
 @main
