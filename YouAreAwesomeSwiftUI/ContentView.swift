8e833a7d566e67d118a700a4b32c7929bd3ac8dc
Samuel Ortega <samy9093@gmail.com>
2023-01-19 18:45:56 -0600

    Ch1.1 Swift UI Basics

--- YouAreAwesomeSwiftUI/ContentView.swift
+++ YouAreAwesomeSwiftUI/ContentView.swift
@@ -3,17 +3,30 @@
 //  YouAreAwesomeSwiftUI
 //
 //  Created by Sammy on 1/19/23.
-//
+// Control+Option+click To pull available modifiers (attribute inspector accomplishes the same thing)
+//Views (elements of app user interface) Always
+//Modifiers accept input values
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "lasso.and.sparkles")
+                .resizable()
+                .scaledToFit()
+                .padding([.bottom, .trailing])
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                
+            Text("H Town, Peace up!")
+            //After pressing enter and pressing "." auto indentation
+                .font(.largeTitle) //Just applied a modifier with an input value
+                .fontWeight(.black)
+            Image(systemName: "swift")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+                .colorInvert()
         }
         .padding()
     }
