472be4c2042e2d7816a5f50517d88edee4b7f1eb
Samuel Ortega <samy9093@gmail.com>
2023-01-19 18:10:34 -0600

    Initial Commit

---
+++ YouAreAwesomeSwiftUI/YouAreAwesomeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeSwiftUIApp.swift
+//  YouAreAwesomeSwiftUI
+//
+//  Created by Sammy on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
